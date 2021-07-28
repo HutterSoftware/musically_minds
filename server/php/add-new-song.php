e9a3dc03a5264f1a1ead9f79aaede07dbf3b83d9
Benedikt Hutter <benedikt.hutter@computer-creativ.com>
2021-07-28 23:24:51 +0200

    feat: Removing debug output

--- server/php/add-new-song.php
+++ server/php/add-new-song.php
@@ -3,12 +3,12 @@
 if (isset($_GET["band-name"])) {
   header("Location: /band-list.php", 200);
   die();
-}echo 1;
+}
 
 if (isset($_GET["new-song-name"])) {
   header("Location: /band.php?band=" . $_GET["band"], 200);
   die();
-}echo 2;
+}
 
 
 require_once("./utils/database.php");
@@ -16,7 +16,6 @@ require_once("./utils/database.php");
 $data = array();
 array_push($data, $_POST["band-name"]);
 
-echo 3;
 $bandId = executeQuery("get-band-id-by-name", "s", $data, DATA_YES);
 
 
